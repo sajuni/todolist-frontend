af7d9058645ee51113b2d8e530f30db352c9bae3
sajuni11 <sajuni11@dotincorp.com>
2023-04-06 19:27:13 +0900

    화면단 작업 중

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-main>
+			<h1>사용자를 먼저 등록해 주세요.</h1>
+			<v-dialog v-model="dialog" width="500">
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+						사용자등록
+					</v-btn>
+				</template>
+
+				<v-card>
+					<v-card-title class="text-h5 grey lighten-2">
+						사용자를 등록해주세요
+					</v-card-title>
+					<v-text-field v-model="name" label="이름" clearable></v-text-field>
+					<v-divider></v-divider>
+
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn color="primary" text @click="save()"> 등록 </v-btn>
+						<v-btn color="primary" text @click="cancel()"> 취소 </v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data() {
+		return {
+			dialog: false,
+			name: "",
+		};
+	},
+	created() {
+		fetch("http://localhost:8080/api/member/12", {
+			method: "GET",
+		})
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	},
+	methods: {
+		save() {
+			const params = new URLSearchParams();
+			params.append("name", this.name);
+			fetch("http://localhost:8080/api/member/save", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: params,
+			})
+				.then(res => {
+					return res.json();
+				})
+				.then(data => {
+					alert(data.name + "님 등록이 완료되었습니다.");
+					this.cancel();
+				})
+				.catch(err => {
+					console.error(err);
+				});
+		},
+		cancel() {
+			this.dialog = false;
+			this.name = "";
+		},
+	},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
